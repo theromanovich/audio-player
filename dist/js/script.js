1eaa442294e189ef6c2292e7ba3c1c5809286398
Vlad Khomenko <vladkhomenko@MacBook-Air-Vlad.local>
2023-01-23 16:35:32 +0200

    add progress bar func

--- dist/js/script.js
+++ dist/js/script.js
@@ -76,7 +76,37 @@ function nextSong() {
 
 loadSong(songs[songIndex]); 
 
+function updateProgressBar(e) {
+    if (isPlaying) {
+        const { duration, currentTime } = e.srcElement;
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+        const durationMinutes = Math.floor(duration/60);
+        let durationSeconds = Math.floor(duration % 60);
+        if (durationSeconds < 10) {
+            durationSeconds = `0${durationSeconds}`;
+        }
+        if (durationSeconds) {
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+        const currentMinutes = Math.floor(currentTime/60);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if (currentSeconds < 10) {
+            currentSeconds = `0${currentSeconds}`;
+        }
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+    }
+}
+
+function setProgressBar(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const {duration} = music;
+    music.currentTime = (clickX / width) * duration;
+}
 
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
 music.addEventListener('ended', nextSong)
+music.addEventListener('timeupdate', updateProgressBar)
+progressContainer.addEventListener('click', setProgressBar)
