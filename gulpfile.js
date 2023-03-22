99a186debe18b53b85e619519b1ce4100921106f
Vlad Khomenko <theromanovich03@gmail.com>
2023-03-22 18:27:34 +0200

    ts transit

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,79 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync');
-const sass = require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
+const autoprefixer = require("gulp-autoprefixer");
 const rename = require("gulp-rename");
-const htmlmin = require('gulp-htmlmin');
-const minify = require('gulp-minify');
+const htmlmin = require("gulp-htmlmin");
+const minify = require("gulp-minify");
+const ts = require("gulp-typescript");
+const tsProject = ts.createProject("tsconfig.json");
 
-gulp.task('server', function() {
+gulp.task("server", function () {
+  browserSync({
+    server: {
+      baseDir: "dist",
+    },
+  });
 
-    browserSync({
-        server: {
-            baseDir: "dist"
-        }
-    });
-
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+  gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
-gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
-        .pipe(gulp.dest("dist/css"))
-        .pipe(browserSync.stream());
+gulp.task("styles", function () {
+  return gulp
+    .src("src/sass/**/*.+(scss|sass)")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(rename({ suffix: ".min", prefix: "" }))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest("src/css"))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
-    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+gulp.task("watch", function () {
+  gulp.watch(
+    ["src/js/**/*.ts", "src/*.html", "src/sass/**/*.+(scss|sass|css)"],
+    gulp.parallel("typescript", "html", "styles")
+  );
+  gulp.watch("dist/js/script.js").on("change", browserSync.reload);
 });
 
-gulp.task('html', function() {
-    return gulp.src("src/*.html")
+gulp.task("html", function () {
+  return gulp
+    .src("src/*.html")
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('dist/'));
-})
+    .pipe(gulp.dest("dist/"));
+});
 
-gulp.task('scripts', function() {
-    return gulp.src("src/js/*.js")
-    .pipe(minify())
-    .pipe(gulp.dest('dist/js'));
-})
+// gulp.task("scripts", function () {
+//   return gulp.src("src/js/*.js").pipe(minify()).pipe(gulp.dest("dist/js"));
+// });
 
-gulp.task('images', function() {
-    return gulp.src("src/img/*")
-    .pipe(gulp.dest('dist/img'));
-})
+gulp.task("images", function () {
+  return gulp.src("src/img/*").pipe(gulp.dest("dist/img"));
+});
 
-gulp.task('music', function() {
-    return gulp.src("src/music/*")
-    .pipe(gulp.dest('dist/music'));
-})
+gulp.task("music", function () {
+  return gulp.src("src/music/*").pipe(gulp.dest("dist/music"));
+});
 
+gulp.task("typescript", function () {
+  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist/js"));
+});
 
-gulp.task('default', gulp.parallel('watch', 'server', 'html', 'styles', 'scripts', 'images', 'music'));
+gulp.task("default", gulp.series("typescript", "watch"));
+
+gulp.task(
+  "default",
+  gulp.parallel(
+    "watch",
+    "server",
+    "html",
+    "styles",
+    // "scripts",
+    "images",
+    "music",
+    "typescript"
+  )
+);
---
+++ src/js/script.ts
@@ -0,0 +1,119 @@
+const image = document.querySelector("img") as HTMLImageElement,
+  title = document.getElementById("title") as HTMLHeadingElement,
+  artist = document.getElementById("artist") as HTMLHeadingElement,
+  music = document.querySelector("audio") as HTMLAudioElement,
+  progressContainer = document.getElementById(
+    "progress-container"
+  ) as HTMLDivElement,
+  progress = document.getElementById("progress") as HTMLDivElement,
+  currentTimeEl = document.getElementById("current-time") as HTMLSpanElement,
+  durationEl = document.getElementById("duration") as HTMLSpanElement,
+  prevBtn = document.getElementById("prev") as HTMLButtonElement,
+  playBtn = document.getElementById("play") as HTMLButtonElement,
+  nextBtn = document.getElementById("next") as HTMLButtonElement;
+
+interface Song {
+  name: string;
+  displayName: string;
+  artist: string;
+}
+
+const songs: Song[] = [
+  {
+    name: "sweet-dreams",
+    displayName: "Sweet Dreams",
+    artist: "Eurythmics",
+  },
+  {
+    name: "paradise",
+    displayName: "Gangsta's Paradise",
+    artist: "Coolio",
+  },
+  {
+    name: "happy-nation",
+    displayName: "Нappy Nation",
+    artist: "Ace Of Base",
+  },
+];
+
+let isPlaying: boolean = false;
+
+function playSong(): void {
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
+  music.play();
+}
+
+function pauseSong(): void {
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+  music.pause();
+}
+
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+function loadSong(song: Song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `../music/${song.name}.mp3`;
+  image.src = `../img/${song.name}.jpeg`;
+}
+
+let songIndex: number = 0;
+
+function prevSong(): void {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function nextSong(): void {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+loadSong(songs[songIndex]);
+
+function updateProgressBar(e: any): void {
+  if (isPlaying) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+    const durationMinutes = Math.floor(duration / 60);
+    let durationSeconds: number | string = Math.floor(duration % 60);
+    if (durationSeconds < 10) {
+      durationSeconds = `0${durationSeconds}`;
+    }
+    if (durationSeconds) {
+      durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+    }
+    const currentMinutes = Math.floor(currentTime / 60);
+    let currentSeconds: number | string = Math.floor(currentTime % 60);
+    if (currentSeconds < 10) {
+      currentSeconds = `0${currentSeconds}`;
+    }
+    currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+  }
+}
+
+function setProgressBar(e: MouseEvent): void {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const { duration } = music;
+  music.currentTime = (clickX / width) * duration;
+}
+
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+music.addEventListener("ended", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
+progressContainer.addEventListener("click", setProgressBar);
